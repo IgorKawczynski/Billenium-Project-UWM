467b24af9eaa797920a610715eeccb938d36fae8
Adam <trentowski.adam@gmail.com>
2023-02-28 19:36:55 +0100

    Adding ColumnRepository (JPA)

--- src/main/java/pl/uwm/projektzespolowy/column/ColumnRepository.java
+++ src/main/java/pl/uwm/projektzespolowy/column/ColumnRepository.java
@@ -1,7 +1,8 @@
 package pl.uwm.projektzespolowy.column;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ColumnRepository {
+public interface ColumnRepository extends JpaRepository<Column, Long> {
 }
