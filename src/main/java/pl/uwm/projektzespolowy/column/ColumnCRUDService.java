f4d0fb10676abf9d05fd3a60474c6fc4c1c6cd4a
Adam <trentowski.adam@gmail.com>
2023-02-28 19:40:05 +0100

    Adding Column service

--- src/main/java/pl/uwm/projektzespolowy/column/ColumnCRUDService.java
+++ src/main/java/pl/uwm/projektzespolowy/column/ColumnCRUDService.java
@@ -2,8 +2,32 @@ package pl.uwm.projektzespolowy.column;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.uwm.projektzespolowy.basic.UpdateDTO;
+import pl.uwm.projektzespolowy.column.dtos.ColumnCreateDTO;
 
 @Service
 @RequiredArgsConstructor
 public class ColumnCRUDService {
+
+    private final AddColumnToBoardService creator;
+    private final ColumnReader reader;
+    private final ColumnUpdater updater;
+    private final ColumnDeleter deleter;
+
+    public Column createColumn(ColumnCreateDTO columnCreateDTO) {
+        return creator.createColumnInPenultimatePosition(columnCreateDTO);
+    }
+
+    public Column getColumnById(Long id) {
+        return reader.getColumnById(id);
+    }
+
+    public void updateColumn(UpdateDTO updateDTO) {
+        updater.updateColumn(updateDTO);
+    }
+
+    public void deleteColumn(Long id) {
+        deleter.deleteColumnById(id);
+    }
+
 }
