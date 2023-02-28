20f659a5d7de4b62e733e5d6c9736e76c9a50d3e
Adam <trentowski.adam@gmail.com>
2023-02-28 19:39:39 +0100

    Adding logic responsible for setting the column to the last position and adding a new one to the penultimate position

---
+++ src/main/java/pl/uwm/projektzespolowy/column/AddColumnToBoardService.java
@@ -0,0 +1,43 @@
+package pl.uwm.projektzespolowy.column;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import pl.uwm.projektzespolowy.board.BoardRepository;
+import pl.uwm.projektzespolowy.column.dtos.ColumnCreateDTO;
+
+import java.util.Comparator;
+import java.util.NoSuchElementException;
+
+import static pl.uwm.projektzespolowy.column.Column.UNLIMITED_SIZE;
+
+@Component
+@RequiredArgsConstructor
+public class AddColumnToBoardService {
+
+    private final ColumnCreator columnCreator;
+    private final ColumnRepository columnRepository;
+    private final BoardRepository boardRepository;
+
+    public Column createColumnInPenultimatePosition(ColumnCreateDTO createDTO) {
+        var maxPositionColumn = getMaxPositionColumnInBoard(createDTO.boardId());
+
+        Integer maxPositionBeforeAddingColumn = maxPositionColumn.getPosition();
+
+        updateIncrementedColumnPosition(maxPositionColumn);
+
+        return columnCreator.createColumn(createDTO.title(), UNLIMITED_SIZE, maxPositionBeforeAddingColumn, createDTO.boardId());
+    }
+
+    private Column getMaxPositionColumnInBoard(Long boardId) {
+        var board = boardRepository.getReferenceById(boardId);
+        var columns = board.getColumns();
+        return columns.stream().max(Comparator.comparing(Column::getPosition)).orElseThrow(NoSuchElementException::new);
+    }
+
+    private void updateIncrementedColumnPosition(Column column) {
+        Integer newMaxPosition = column.getPosition() + 1;
+        column.setPosition(newMaxPosition);
+        columnRepository.saveAndFlush(column);
+    }
+
+}
