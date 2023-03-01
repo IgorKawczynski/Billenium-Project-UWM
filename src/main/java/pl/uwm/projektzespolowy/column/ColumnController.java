944b99abf78808bd2a2a1b9872a58e0adddb037b
Adam <trentowski.adam@gmail.com>
2023-03-01 17:51:28 +0100

    Adding CRUD rests to ColumnController

--- src/main/java/pl/uwm/projektzespolowy/column/ColumnController.java
+++ src/main/java/pl/uwm/projektzespolowy/column/ColumnController.java
@@ -1,8 +1,10 @@
 package pl.uwm.projektzespolowy.column;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import pl.uwm.projektzespolowy.basic.UpdateDTO;
+import pl.uwm.projektzespolowy.column.dtos.ColumnCreateDTO;
+import pl.uwm.projektzespolowy.column.dtos.ColumnDTO;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,6 +13,24 @@ public class ColumnController {
 
     private final ColumnCRUDService service;
 
-//    TODO: dto czy encja
+    @PostMapping("")
+    public ColumnDTO createColumn(@RequestBody ColumnCreateDTO columnCreateDTO) {
+        return service.createColumn(columnCreateDTO).toDto();
+    }
+
+    @GetMapping("/{columnId}")
+    public ColumnDTO getColumnById(@PathVariable Long columnId) {
+        return service.getColumnById(columnId).toDto();
+    }
+
+    @PatchMapping("")
+    public void updateColumn(@RequestBody UpdateDTO updateDTO) {
+        service.updateColumn(updateDTO);
+    }
+
+    @DeleteMapping("/{columnId}")
+    public void deleteColumnById(@PathVariable Long columnId) {
+        service.deleteColumn(columnId);
+    }
 
 }
