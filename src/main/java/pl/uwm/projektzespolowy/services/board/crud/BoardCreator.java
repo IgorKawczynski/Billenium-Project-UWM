ee83468be0b5bcc8ab936278c755f198f07bc9dc
krychuu01 <hazzidy125@gmail.com>
2023-03-03 20:30:48 +0100

    Code refactoring in BoardCreator class

--- src/main/java/pl/uwm/projektzespolowy/services/board/crud/BoardCreator.java
+++ src/main/java/pl/uwm/projektzespolowy/services/board/crud/BoardCreator.java
@@ -5,17 +5,17 @@ import org.springframework.stereotype.Component;
 import pl.uwm.projektzespolowy.models.board.Board;
 import pl.uwm.projektzespolowy.models.valueobjects.Title;
 import pl.uwm.projektzespolowy.services.board.BoardRepository;
-import pl.uwm.projektzespolowy.services.user.UserRepository;
+import pl.uwm.projektzespolowy.services.user.crud.UserReader;
 
 @Component
 @RequiredArgsConstructor
 public class BoardCreator {
 
-    private final UserRepository userRepository;
+    private final UserReader userReader;
     private final BoardRepository boardRepository;
 
     public Board createBoard(String givenTitle, Long ownerId) {
-        var boardOwner = userRepository.getReferenceById(ownerId);
+        var boardOwner = userReader.getUserById(ownerId);
         var title = new Title(givenTitle);
         return boardRepository.saveAndFlush(new Board(title, boardOwner));
     }
