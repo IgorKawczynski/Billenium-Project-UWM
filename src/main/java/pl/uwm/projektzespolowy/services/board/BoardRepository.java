07e2bbd0a9f35df2baa42698cb523ea468e7ccba
IgorKawczynski <kluseczkibabuni94@gmail.com>
2023-03-05 23:25:39 +0100

    Added Repository annotation for BoardRepository interface...

--- src/main/java/pl/uwm/projektzespolowy/services/board/BoardRepository.java
+++ src/main/java/pl/uwm/projektzespolowy/services/board/BoardRepository.java
@@ -1,7 +1,9 @@
 package pl.uwm.projektzespolowy.services.board;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pl.uwm.projektzespolowy.models.board.Board;
 
+@Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
 }
