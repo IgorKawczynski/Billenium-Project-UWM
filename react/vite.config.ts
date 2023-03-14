f4ac646d9fb05bc2014d4cff0f730bf0358268e5
IgorKawczynski <kluseczkibabuni94@gmail.com>
2023-03-14 19:07:13 +0100

    Modified vite configuration file for Railway Cloud compability #4

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dns from 'dns'
+
+dns.setDefaultResultOrder('verbatim')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    hmr: {
-      host: '172.17.2.33',
-    },
+      host: true,
   }
 })
