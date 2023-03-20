6626a69a04890325957a34b067d04f341df71a12
IgorKawczynski <kluseczkibabuni94@gmail.com>
2023-03-20 22:41:53 +0100

    #71 - Added Row entity

---
+++ src/main/java/pl/uwm/projektzespolowy/models/row/Row.java
@@ -0,0 +1,66 @@
+package pl.uwm.projektzespolowy.models.row;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+import pl.uwm.projektzespolowy.models.Positionable;
+import pl.uwm.projektzespolowy.models.basic.BasicEntity;
+import pl.uwm.projektzespolowy.models.board.Board;
+import pl.uwm.projektzespolowy.models.cell.Cell;
+import pl.uwm.projektzespolowy.models.valueobjects.Position;
+import pl.uwm.projektzespolowy.models.valueobjects.Title;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "rows")
+@Setter
+@Getter
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Row extends BasicEntity implements Positionable {
+
+    Title title;
+    Position position;
+
+    @ManyToOne
+    @JoinColumn(name = "board_id", referencedColumnName = "id")
+    Board board;
+
+    @OneToMany(mappedBy = "row", cascade = CascadeType.ALL, orphanRemoval = true)
+    List<Cell> cells;
+
+    public Row(Title title, Position position, Board board) {
+        this.title = title;
+        this.position = position;
+        this.board = board;
+        this.cells = new ArrayList<>();
+    }
+
+    public RowResponseDTO toDto() {
+        return RowResponseDTO.builder()
+                .id(this.id.toString())
+                .title(this.title.toString())
+                .position(this.position.value())
+                .build();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof Row))
+            return false;
+        return this.id != null && this.id.equals(((Row) o).getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+
+}
