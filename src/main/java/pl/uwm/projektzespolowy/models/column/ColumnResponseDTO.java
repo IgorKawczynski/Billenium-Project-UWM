e40469885969e01ad79cf95a17d16bea3e502e65
IgorKawczynski <kluseczkibabuni94@gmail.com>
2023-03-20 22:03:04 +0100

    #70 - Modified column DTOs to consume cells instead of cards

--- src/main/java/pl/uwm/projektzespolowy/models/column/ColumnResponseDTO.java
+++ src/main/java/pl/uwm/projektzespolowy/models/column/ColumnResponseDTO.java
@@ -1,7 +1,7 @@
 package pl.uwm.projektzespolowy.models.column;
 
 import lombok.Builder;
-import pl.uwm.projektzespolowy.models.card.CardResponseDTO;
+import pl.uwm.projektzespolowy.models.cell.CellResponseDTO;
 
 import java.util.List;
 
@@ -9,7 +9,7 @@ public record ColumnResponseDTO(String id,
                                 String title,
                                 Integer cardsLimit,
                                 Integer position,
-                                List<CardResponseDTO> cards) {
+                                List<CellResponseDTO> cells) {
 
     @Builder public ColumnResponseDTO {}
 
