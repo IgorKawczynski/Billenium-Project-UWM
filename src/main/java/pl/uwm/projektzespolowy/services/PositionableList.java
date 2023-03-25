931e8c12aa27f2c8c978d285008b4f4c24cbd47f
krychuu01 <hazzidy125@gmail.com>
2023-03-25 20:36:23 +0100

    Added custom comparator to PositionableList and getPreviousElement method

--- src/main/java/pl/uwm/projektzespolowy/services/PositionableList.java
+++ src/main/java/pl/uwm/projektzespolowy/services/PositionableList.java
@@ -3,6 +3,7 @@ package pl.uwm.projektzespolowy.services;
 import pl.uwm.projektzespolowy.models.Positionable;
 import pl.uwm.projektzespolowy.models.valueobjects.Position;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.function.Consumer;
 
@@ -58,6 +59,18 @@ public class PositionableList<T extends Positionable> {
         if (newPosition.value() > currentPosition.value()) {
             this.withPositionInRange(currentPosition, newPosition);
             this.moveLeftAll();
+        }
+    }
+
+    public T getPreviousElement(T positionable) {
+        this.positionables.sort(new PositionableComparator()); // not sure if necessary
+        return this.positionables.get(positionable.getPosition().value() - 1);
+    }
+
+    private static class PositionableComparator implements Comparator<Positionable> {
+        @Override
+        public int compare(Positionable first, Positionable second) {
+            return first.getPosition().compareTo(second.getPosition());
         }
     }
 
