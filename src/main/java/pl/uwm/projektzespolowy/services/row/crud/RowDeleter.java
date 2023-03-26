35835a77edba1c3a85c6f13550f7dc81e2b0b7ab
krychuu01 <hazzidy125@gmail.com>
2023-03-26 20:24:09 +0200

    #82 changed RowDeleter implementation, added new exception and handler for it

--- src/main/java/pl/uwm/projektzespolowy/services/row/crud/RowDeleter.java
+++ src/main/java/pl/uwm/projektzespolowy/services/row/crud/RowDeleter.java
@@ -2,7 +2,10 @@ package pl.uwm.projektzespolowy.services.row.crud;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import pl.uwm.projektzespolowy.exceptions.RowCantBeDeletedException;
 import pl.uwm.projektzespolowy.models.board.Board;
+import pl.uwm.projektzespolowy.models.cell.Cell;
+import pl.uwm.projektzespolowy.models.column.Column;
 import pl.uwm.projektzespolowy.models.row.Row;
 import pl.uwm.projektzespolowy.services.PositionableList;
 
@@ -13,30 +16,49 @@ class RowDeleter {
     private final RowRepository rowRepository;
 
     public void deleteRow(Board board, Row rowToDelete) {
-
         var boardRows = new PositionableList<>(board.getRows());
+        validateIfRowCanBeDeleted(boardRows, rowToDelete);
         boardRows.withHigherOrEqualPositionThanGiven(rowToDelete);
         boardRows.moveLeftAll();
+        var boardColumns = new PositionableList<>(board.getColumns());
+        cutCardsFromRowToDeleteToLastRow(boardColumns, rowToDelete);
 
-        for(int i=0; i<board.getColumns().size(); i++) {
+        for(Column column: boardColumns) {
+            var boardCells = new PositionableList<>(column.getCells());
+            boardCells.withHigherOrEqualPositionThanGiven(boardCells.get(rowToDelete.getPosition().value()));
+            boardCells.moveLeftAll();
 
-            if(boardRows.list().size() > 0) {
-                var boardCells = new PositionableList<>(board.getColumns().get(i).getCells());
-                boardCells.withHigherOrEqualPositionThanGiven(board.getColumns().get(i).getCells().get(rowToDelete.getPosition().value()));
-                boardCells.moveLeftAll();
-            }
-
-            board
-             .getColumns()
-             .get(i)
-             .getCells()
-             .remove(rowToDelete.getPosition().value());
-
+            column.getCells()
+                    .remove(rowToDelete.getPosition().value());
         }
 
         board.deleteRow(rowToDelete);
         rowRepository.delete(rowToDelete);
         rowRepository.saveAll(boardRows.list());
-
     }
+
+    private void validateIfRowCanBeDeleted(PositionableList<Row> boardRows, Row rowToDelete) {
+        if (rowToDeleteIsLastBoardRow(boardRows, rowToDelete)) {
+            throw new RowCantBeDeletedException("Last board row can not be deleted.");
+        }
+    }
+
+    private boolean rowToDeleteIsLastBoardRow(PositionableList<Row> boardRows, Row rowToDelete) {
+        var lastBoardRow = boardRows.getLastElement();
+        return rowToDelete.getPosition().compareTo(lastBoardRow.getPosition()) == 0;
+    }
+
+    private void cutCardsFromRowToDeleteToLastRow(PositionableList<Column> boardColumns, Row rowToDelete) {
+        for (Column column: boardColumns) {
+            var columnCells = new PositionableList<>(column.getCells());
+            var cellWithPositionSameAsRowToDelete = columnCells.get(rowToDelete.getPosition().value());
+            var lastColumnCell = columnCells.getLastElement();
+            moveCardsFromOneCellToAnother(lastColumnCell, cellWithPositionSameAsRowToDelete);
+        }
+    }
+
+    private void moveCardsFromOneCellToAnother(Cell firstCell, Cell secondCell) {
+        firstCell.addAll(secondCell.getCards());
+    }
+
 }
