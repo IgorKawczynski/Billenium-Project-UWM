dc3992e0e3dd58bc44c0e379793a2d0b498307dc
krychuu01 <hazzidy125@gmail.com>
2023-03-26 20:23:34 +0200

    #82 improved ColumnDeleter implementation

--- src/main/java/pl/uwm/projektzespolowy/services/column/crud/ColumnDeleter.java
+++ src/main/java/pl/uwm/projektzespolowy/services/column/crud/ColumnDeleter.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import pl.uwm.projektzespolowy.exceptions.BoardHasTooFewColumnsToDeleteException;
 import pl.uwm.projektzespolowy.exceptions.ColumnCantBeDeletedException;
-import pl.uwm.projektzespolowy.exceptions.VOExceptions.ColumnDoesNotExistsException;
 import pl.uwm.projektzespolowy.models.board.Board;
 import pl.uwm.projektzespolowy.models.cell.Cell;
 import pl.uwm.projektzespolowy.models.column.Column;
@@ -17,8 +16,8 @@ class ColumnDeleter {
     private final ColumnRepository columnRepository;
 
     public void deleteColumn(Column columnToDelete, Board board) {
-        validateIfColumnCanBeDeleted(board, columnToDelete);
         var boardColumns = new PositionableList<>(board.getColumns());
+        validateIfColumnCanBeDeleted(board, boardColumns, columnToDelete);
         var previousColumn = boardColumns.getPreviousElement(columnToDelete);
         cutCardsFromColumnToDeleteToPreviousColumn(previousColumn, columnToDelete);
         boardColumns.withHigherOrEqualPositionThanGiven(columnToDelete);
@@ -28,14 +27,14 @@ class ColumnDeleter {
         columnRepository.saveAll(boardColumns.list());
     }
 
-    public void validateIfColumnCanBeDeleted(Board board, Column columnToDelete) {
+    public void validateIfColumnCanBeDeleted(Board board, PositionableList<Column> boardColumns, Column columnToDelete) {
         if (board.getColumns().size() <= 2) {
             throw new BoardHasTooFewColumnsToDeleteException("Board must have at least two columns.");
         }
-        if (columnToDeleteIsFirstBoardColumn(board, columnToDelete)) {
+        if (columnToDeleteIsFirstBoardColumn(boardColumns, columnToDelete)) {
             throw new ColumnCantBeDeletedException("First column can not be deleted.");
         }
-        if (columnToDeleteIsLastBoardColumn(board, columnToDelete)) {
+        if (columnToDeleteIsLastBoardColumn(boardColumns, columnToDelete)) {
             throw new ColumnCantBeDeletedException("Last column can not be deleted.");
         }
     }
@@ -50,21 +49,13 @@ class ColumnDeleter {
         }
     }
 
-    private boolean columnToDeleteIsLastBoardColumn(Board board, Column columnToDelete) {
-        var lastColumn = board.getColumns()
-                .stream()
-                .filter(column -> column.getPosition().value() == board.getColumns().size() - 1)
-                .findFirst()
-                .orElseThrow(() -> new ColumnDoesNotExistsException("Board doesn't have column with first position."));
+    private boolean columnToDeleteIsLastBoardColumn(PositionableList<Column> boardColumns, Column columnToDelete) {
+        var lastColumn = boardColumns.getLastElement();
         return lastColumn.getPosition().compareTo(columnToDelete.getPosition()) == 0;
     }
 
-    private boolean columnToDeleteIsFirstBoardColumn(Board board, Column columnToDelete) {
-        var firstColumn = board.getColumns()
-                .stream()
-                .filter(column -> column.getPosition().value() == 0)
-                .findFirst()
-                .orElseThrow(() -> new ColumnDoesNotExistsException("Board doesn't have column with last position."));
+    private boolean columnToDeleteIsFirstBoardColumn(PositionableList<Column> boardColumns, Column columnToDelete) {
+        var firstColumn = boardColumns.getFirstElement();
         return firstColumn.getPosition().compareTo(columnToDelete.getPosition()) == 0;
     }
 
