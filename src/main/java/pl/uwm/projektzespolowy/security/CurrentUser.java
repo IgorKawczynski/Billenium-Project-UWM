c92ee90702b1401f27d76af0d6f7baa8f67c6b7a
IgorKawczynski <kluseczkibabuni94@gmail.com>
2023-03-26 20:17:53 +0200

    #22 - Added CurrentUser class and service to load actually logged user in session

---
+++ src/main/java/pl/uwm/projektzespolowy/security/CurrentUser.java
@@ -0,0 +1,67 @@
+package pl.uwm.projektzespolowy.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class CurrentUser implements UserDetails {
+
+    private Long id;
+    private String email;
+    private String password;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("CREATOR"));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
