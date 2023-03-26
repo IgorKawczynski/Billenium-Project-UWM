48ad45046e0de77fd6281472b11c286273507412
IgorKawczynski <kluseczkibabuni94@gmail.com>
2023-03-26 20:21:58 +0200

    #22 - Removed old ValidatorService and unnecessary exceptions

--- src/main/java/pl/uwm/projektzespolowy/exceptions/VOExceptions/IntegerValidatorException.java
+++
@@ -1,9 +0,0 @@
-package pl.uwm.projektzespolowy.exceptions.VOExceptions;
-
-public class IntegerValidatorException extends RuntimeException{
-
-    public IntegerValidatorException(String message) {
-        super(message);
-    }
-
-}
--- src/main/java/pl/uwm/projektzespolowy/exceptions/VOExceptions/StringValidatorException.java
+++
@@ -1,9 +0,0 @@
-package pl.uwm.projektzespolowy.exceptions.VOExceptions;
-
-public class StringValidatorException extends RuntimeException{
-
-    public StringValidatorException(String message) {
-        super(message);
-    }
-
-}
