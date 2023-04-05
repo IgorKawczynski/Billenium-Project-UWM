89db6209a09ca9831aa6c5662153c5e2fa0461d8
krychuu01 <hazzidy125@gmail.com>
2023-04-06 00:32:39 +0200

    #65 added/updated Card classes tests

--- src/test/java/pl/uwm/projektzespolowy/services/card/CardFacadeTest.java
+++ src/test/java/pl/uwm/projektzespolowy/services/card/CardFacadeTest.java
@@ -7,6 +7,9 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import pl.uwm.projektzespolowy.models.card.Card;
 import pl.uwm.projektzespolowy.models.card.CardCreateDTO;
+import pl.uwm.projektzespolowy.models.card.CardResponseDTO;
+import pl.uwm.projektzespolowy.models.card.CardUpdateDTO;
+import pl.uwm.projektzespolowy.models.user.UserResponseDTO;
 import pl.uwm.projektzespolowy.services.board.crud.BoardCRUDService;
 import pl.uwm.projektzespolowy.services.card.crud.CardCRUDService;
 import pl.uwm.projektzespolowy.services.cell.crud.CellCRUDService;
@@ -14,8 +17,7 @@ import pl.uwm.projektzespolowy.services.user.crud.UserCRUDService;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
-import static pl.uwm.projektzespolowy.services.card.CardTestUtils.createCardWithEveryField;
-import static pl.uwm.projektzespolowy.services.card.CardTestUtils.createCellWithCards;
+import static pl.uwm.projektzespolowy.services.card.CardTestUtils.*;
 
 @ExtendWith(MockitoExtension.class)
 public class CardFacadeTest {
@@ -41,7 +43,7 @@ public class CardFacadeTest {
         var description = "card description";
         var cardCreateDTO = new CardCreateDTO(cellId, title, description);
         var cell = createCellWithCards();
-        Card createdCard = createCardWithEveryField(title, description);
+        Card createdCard = createCardWithEveryField(1L, title, description, 0);
         // when
         when(cellCRUDService.getCellById(Long.parseLong(cellId))).thenReturn(cell);
         when(cardCRUDService.createCard(cell, title, description)).thenReturn(createdCard);
@@ -57,7 +59,7 @@ public class CardFacadeTest {
         Long cardId = 1L;
         var title = "card title";
         var description = "card description";
-        var card = createCardWithEveryField(title, description);
+        var card = createCardWithEveryField(1L, title, description, 0);
         // when
         when(cardCRUDService.getCardById(cardId)).thenReturn(card);
         var foundCard = cardFacade.getCardById(cardId);
@@ -67,16 +69,45 @@ public class CardFacadeTest {
         assertThat(foundCard.description()).isEqualTo(description);
     }
 
-//    @Test
-//    void shouldGetAllCardsByCellId() {
-//        // given
-//        var cell = createCellWithCards();
-//        // when
-//        when(cardCRUDService.getAllCardsByCellId(cell.getId())).thenReturn(cell.getCards());
-//        var cellCards = cardFacade.getAllCardsByCellId(cell.getId());
-//        // then
-//        assertThat(cellCards).hasSize(5);
-//    }
+    @Test
+    void shouldGetAllCardsByCellId() {
+        // given
+        var cell = createCellWithCards();
+        // when
+        when(cardCRUDService.getAllCardsByCellId(cell.getId())).thenReturn(cell.getCards());
+        var cellCards = cardFacade.getAllCardsByCellId(cell.getId());
+        // then
+        assertThat(cellCards).hasSize(5);
+    }
+
+    @Test
+    void shouldReturnAllAssignedUsersToCard() {
+        // given
+        var card = createCardWithAssignedUsers(1L, 0);
+        // when
+        when(cardCRUDService.getAllAssignedUsersToCard(card.getId())).thenReturn(card.getAssignedUsers().stream().toList());
+        var assignedUsers = cardFacade.getAllAssignedUsersToCard(card.getId());
+        // then
+        assertThat(assignedUsers).hasSize(3);
+        assertThat(assignedUsers.stream().findAny().get()).isInstanceOf(UserResponseDTO.class);
+    }
+
+    @Test
+    void shouldUpdateCard() {
+        // given
+        Long cardId = 1L;
+        var newTitle = "some nice title";
+        var newDescription = "some good description here";
+        var cardUpdateDTO = new CardUpdateDTO(cardId.toString(), newTitle, newDescription);
+        var card = createCardWithEveryField(cardId, newTitle, newDescription, 0);
+        // when
+        when(cardCRUDService.updateCard(cardId, newTitle, newDescription)).thenReturn(card);
+        var updatedCard = cardFacade.updateCard(cardUpdateDTO);
+        // then
+        assertThat(updatedCard.id()).isEqualTo("1");
+        assertThat(updatedCard.title()).isEqualTo("some nice title");
+        assertThat(updatedCard).isInstanceOf(CardResponseDTO.class);
+    }
 
 
 }
