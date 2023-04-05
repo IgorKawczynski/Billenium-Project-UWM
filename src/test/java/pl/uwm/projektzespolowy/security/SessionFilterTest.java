0cdbff8793759c6bfca6f90e0e2a61e1a22df0d7
IgorKawczynski <kluseczkibabuni94@gmail.com>
2023-04-05 23:33:33 +0200

    #65 - added unit test for SessionFilter

---
+++ src/test/java/pl/uwm/projektzespolowy/security/SessionFilterTest.java
@@ -0,0 +1,40 @@
+package pl.uwm.projektzespolowy.security;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.mock.web.MockFilterChain;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import static org.mockito.Mockito.*;
+
+class SessionFilterTest {
+
+    @Test
+    void filterInternalWithValidSessionTokenShouldSetAuthenticationInSecurityContextHolder() throws Exception {
+        // when
+        final String sessionId = "valid-session-token";
+        final String email = "test@example.com";
+        final CurrentUser currentUser = mock(CurrentUser.class);
+        final SessionRegistry sessionRegistry = mock(SessionRegistry.class);
+        final CurrentUserService currentUserService = mock(CurrentUserService.class);
+        final SessionFilter sessionFilter = new SessionFilter(sessionRegistry, currentUserService);
+        final MockHttpServletRequest request = new MockHttpServletRequest();
+        final MockHttpServletResponse response = new MockHttpServletResponse();
+        final MockFilterChain filterChain = new MockFilterChain();
+        final UsernamePasswordAuthenticationToken expectedAuthentication = new UsernamePasswordAuthenticationToken(
+                currentUser,
+                null
+        );
+        when(sessionRegistry.getEmailForSession(sessionId)).thenReturn(email);
+        when(currentUserService.loadUserByUsername(email)).thenReturn(currentUser);
+        // given
+        request.addHeader("Authorization", sessionId);
+        sessionFilter.doFilterInternal(request, response, filterChain);
+        // then
+        assertNotEquals(expectedAuthentication, SecurityContextHolder.getContext().getAuthentication());
+    }
+}
