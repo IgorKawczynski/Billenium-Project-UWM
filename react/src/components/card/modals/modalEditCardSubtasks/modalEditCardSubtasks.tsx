75c4ebb7b28a97a0b1a46af65702c0518b5ea790
MaciekMakowski <mvciek.mvkowski@gmail.com>
2023-04-17 12:07:44 +0200

    #116 Hide subtask completeness bar if its empty

--- react/src/components/card/modals/modalEditCardSubtasks/modalEditCardSubtasks.tsx
+++ react/src/components/card/modals/modalEditCardSubtasks/modalEditCardSubtasks.tsx
@@ -10,46 +10,62 @@ const ModalEditCardSubtasks = (props:ModalEditCardSubtasksProps) =>{
     const theme = useTheme()
 
     return(
-        <Box
-            minHeight={'40px'}
-            aria-label={'checkboxField'}
-            display={"flex"}
-            flexDirection={"column"}
-            textAlign={"left"}
-        >
-            <Box>
-                <Typography variant={"subtitle1"} sx={{color:theme.palette.text.primary}}>
-                    Subtasks
-                </Typography>
-                <CardPercentageCompletedLinear subtasks={props.subtasks}/>
-            </Box>
-            <Stack
-                padding={1}
-                spacing={1}
-                direction={"column"}
-                maxHeight={'200px'}
-                overflow={"auto"}
-            >
-                <ModalEditCardSubtasksAddButton cardId={props.cardId} data={props.data} setData={props.setData}/>
-                {props.subtasks.map(subtask => {
-                    return(
-                    <ModalEditCardSubtasksItem
-                        key={subtask.id}
-                        cardId={props.cardId}
-                        cardTitle={props.cardTitle}
-                        isChecked={subtask.isChecked}
-                        title={subtask.title}
-                        id={subtask.id}
-                        data={props.data}
-                        setData={props.setData}
-                        window={props.window}
-                    />
-                    )
-                })}
+        <>
+            {props.subtasks.length > 0 && (
+                <Box
+                    minHeight={'40px'}
+                    aria-label={'checkboxField'}
+                    display={"flex"}
+                    flexDirection={"column"}
+                    textAlign={"left"}
+                >
+                    <Box>
+                        <Typography variant={"subtitle1"} sx={{color:theme.palette.text.primary}}>
+                            Subtasks
+                        </Typography>
+                        <CardPercentageCompletedLinear subtasks={props.subtasks}/>
+                    </Box>
+                    <Stack
+                        padding={1}
+                        spacing={1}
+                        direction={"column"}
+                        maxHeight={'200px'}
+                        overflow={"auto"}
+                    >
+                        <ModalEditCardSubtasksAddButton cardId={props.cardId} data={props.data} setData={props.setData}/>
+                        {props.subtasks.map(subtask => {
+                            return(
+                                <ModalEditCardSubtasksItem
+                                    key={subtask.id}
+                                    cardId={props.cardId}
+                                    cardTitle={props.cardTitle}
+                                    isChecked={subtask.isChecked}
+                                    title={subtask.title}
+                                    id={subtask.id}
+                                    data={props.data}
+                                    setData={props.setData}
+                                    window={props.window}
+                                />
+                            )
+                        })}
 
-            </Stack>
+                    </Stack>
 
-        </Box>
+                </Box>
+            )}
+            {props.subtasks.length == 0 && (
+                <Stack
+                    padding={1}
+                    spacing={1}
+                    direction={"column"}
+                    maxHeight={'200px'}
+                    overflow={"auto"}
+                >
+                    <ModalEditCardSubtasksAddButton cardId={props.cardId} data={props.data} setData={props.setData}/>
+                </Stack>
+            )}
+
+        </>
     )
 }
 
