21e099bcc03aa503575196c5f4345849d0f55b45
Adam <trentowski.adam@gmail.com>
2023-05-15 19:27:36 +0200

    #120 Added query to getCardChildren

--- src/main/java/pl/uwm/projektzespolowy/services/card/crud/CardRepository.java
+++ src/main/java/pl/uwm/projektzespolowy/services/card/crud/CardRepository.java
@@ -1,6 +1,8 @@
 package pl.uwm.projektzespolowy.services.card.crud;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pl.uwm.projektzespolowy.models.card.Card;
 
@@ -10,5 +12,12 @@ import java.util.List;
 interface CardRepository extends JpaRepository<Card, Long> {
 
     List<Card> findAllByCellId(Long cellId);
+
+    @Query("""
+            select c
+            from Card c
+            where c.parentCardId = :parentId
+            """)
+    List<Card> getCardChildren(@Param("parentId") Long parentId);
 
 }
