2f61bd2d9d8629a25ad21d8bf213f7cfe4b434b5
Adam <trentowski.adam@gmail.com>
2023-05-30 22:42:07 +0200

    Changed cors conf

--- src/main/java/pl/uwm/projektzespolowy/ProjektzespolowyApplication.java
+++ src/main/java/pl/uwm/projektzespolowy/ProjektzespolowyApplication.java
@@ -20,7 +20,7 @@ public class ProjektzespolowyApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("https://igor121681.smallhost.pl", "https://igor121681.smallhost.pl/**"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("https://react-frontend-production-8f21.up.railway.app", "https://react-frontend-production-8f21.up.railway.app/**"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
@@ -31,4 +31,5 @@ public class ProjektzespolowyApplication {
 		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
 		return new CorsFilter(urlBasedCorsConfigurationSource);
 	}
+
 }
