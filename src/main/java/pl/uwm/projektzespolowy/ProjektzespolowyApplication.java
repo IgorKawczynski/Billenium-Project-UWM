6f23a316eef147aa004a1d75b63e16366d0a3922
Adam <trentowski.adam@gmail.com>
2023-05-31 20:06:21 +0200

    Changed cors

--- src/main/java/pl/uwm/projektzespolowy/ProjektzespolowyApplication.java
+++ src/main/java/pl/uwm/projektzespolowy/ProjektzespolowyApplication.java
@@ -20,7 +20,8 @@ public class ProjektzespolowyApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("https://magikanban.up.railway.app", "https://magikanban.up.railway.app/**"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("https://magikanban.up.railway.app", "https://magikanban.up.railway.app/**",
+				"http://igor121681.smallhost.pl", "http://igor121681.smallhost.pl/**"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
