36c86b67819a93c3b76248b6c2108777813ad5de
MaciekMakowski <mvciek.mvkowski@gmail.com>
2023-05-31 17:51:57 +0200

    Change link in backend to new front

--- src/main/java/pl/uwm/projektzespolowy/ProjektzespolowyApplication.java
+++ src/main/java/pl/uwm/projektzespolowy/ProjektzespolowyApplication.java
@@ -20,7 +20,7 @@ public class ProjektzespolowyApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("https://react-frontend-production-cfc6.up.railway.app/", "https://react-frontend-production-cfc6.up.railway.app//**"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("https://react-frontend-production-cfc6.up.railway.app/", "https://react-frontend-production-cfc6.up.railway.app/**"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
